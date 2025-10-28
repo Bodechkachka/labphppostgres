5d412d66ae5356040216acf149283291c026f8e7
Bogdan Kuzenkov <kuzenkovbk1@gmail.com>
2025-10-28 14:24:04 +0300

    get_all.php
    asd

--- get_all.php
+++ get_all.php
@@ -1,31 +1,34 @@
 <?php
-$host = "127.0.1.23";
-$port = "5432";
-$dbname = "mydatabase";
-$user = "postgres1";
-$password = "kSOFQ88UW4eISRBOINpqY2ELCNRGrin3";
+$host = getenv('PGHOST');
+$port = getenv('PGPORT');
+$dbname = getenv('PGDATABASE');
+$user = getenv('PGUSER');
+$password = getenv('PGPASSWORD');
 
 $conn_string = "host=$host port=$port dbname=$dbname user=$user password=$password";
 $conn = pg_connect($conn_string);
 
 if (!$conn) {
-    die("Ошибка подключения к PostgreSQL");
+    http_response_code(500);
+    echo json_encode(["error" => "Ошибка подключения к базе"]);
+    exit;
 }
 
 pg_set_client_encoding($conn, "UTF8");
 
-// Получаем все 14 дней
-$query = "SELECT holiday_date, text FROM tab ORDER BY holiday_date LIMIT 14;";
+$query = "SELECT holiday_date, text FROM tab ORDER BY holiday_date";
 $result = pg_query($conn, $query);
 
-$holidays = [];
-while ($row = pg_fetch_assoc($result)) {
-    $holidays[] = $row;
+$data = [];
+if ($result) {
+    while ($row = pg_fetch_assoc($result)) {
+        $data[] = $row;
+    }
 }
 
-pg_close($conn);
-
-// Отдаём JSON
 header('Content-Type: application/json');
-echo json_encode($holidays, JSON_UNESCAPED_UNICODE);
+echo json_encode($data);
+
+pg_free_result($result);
+pg_close($conn);
 ?>
